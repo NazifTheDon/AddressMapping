a6df976b26e2d08d1a733538d9157edff8b13386
NazifTheDon <70698290+NazifTheDon@users.noreply.github.com>
2022-11-18 15:48:03 -0800

    Add files via upload

--- map.py
+++ map.py
@@ -4,33 +4,35 @@ import webbrowser
 from main import extract_lang_long
 import csv
 
-class Map:
-    def __init__(self, location, zoom_start):
-        self.location = location
-        self.zoom_start = zoom_start
 
-    def showMap(self):
-        # Create the map
-        my_map = folium.Map(location=self.location, zoom_start=self.zoom_start)
-
-        # Display the map
-        my_map.save("map.html")
-        webbrowser.open("map.html")
-
-places = {'cities': ["10915 Jasmine Crest Lane", "1600 Amphitheatre Parkway, Mountain View, CA"],
-          'latitude': [32.9165762, 37.4223878],
-          'longitude': [-117.1883953, -122.0841877]}
+#places = {'cities': ["10915 Jasmine Crest Lane", "1600 Amphitheatre Parkway, Mountain View, CA"],
+#          'latitude': [32.9165762, 37.4223878],
+#          'longitude': [-117.1883953, -122.0841877],
+#          'called': ['yes', 'no']}
+#          #'visited': ['yes', 'yes']}
 
 
-df = pd.DataFrame(places, columns=['cities', 'latitude', 'longitude']).to_csv('places.csv')
+#df = pd.DataFrame(places, columns=['cities', 'latitude', 'longitude', 'called']).to_csv('places.csv')
 
-address = pd.read_csv('places.csv')
-print(address.head())
+address = pd.read_excel('Jolla 5 Primary Parcels.xlsx')
+
+def marker_color(row):
+    if row['called'] == 'yes':
+        return 'green'
+    #elif row['lived'] == 'no' and row ['visited'] == 'yes':
+    #    return 'purple'
+    return 'red'
+
+#address['color'] = address.apply(marker_color, axis=1)
+
 center = [47.116386, -101.299591]
-map_USA = folium.Map(location=center, zoom_start=3)
-for index, place in address.iterrows():
-    location = [place['latitude'], place['longitude']]
-    folium.Marker(location, popup = f'Address: {place["cities"]}').add_to(map_USA)
+map_USA = folium.Map(location=center, zoom_start=4)
+for _, place in address.iterrows():
+    if None not in extract_lang_long(place['MAIL_ADDRE']):
+        location = extract_lang_long(place['MAIL_ADDRE'])
+        folium.Marker(location, popup=f'{place["MAIL_NAME"]}', tooltip=place['MAIL_NAME'], icon=folium.Icon(prefix='fa', icon='circle')).add_to(map_USA)
+
+
 
 # save map to html file
 map_USA.save('index.html')
